cb09ab3c335ce86403ff1bbd32a8ec2e1a750661
092Usman <87968718+092Usman@users.noreply.github.com>
2022-01-24 07:45:53 +0500

    Add files via upload

---
+++ bain/page.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+
+<article class="row column">
+  <div class="column">
+    <h1>page.php</h1>
+    <?php the_content()?>
+  </div>
+</article>
+
+
+<?php get_footer(); ?>
---
+++ bain/single.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<article class="row column">
+  <div class="column">
+    <?php the_content()?>
+  </div>
+</article>
+
+
+<?php get_footer(); ?>
